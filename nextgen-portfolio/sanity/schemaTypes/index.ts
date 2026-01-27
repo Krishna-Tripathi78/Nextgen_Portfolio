3cd404448483ec8b0941519ab130c88ccb23e721
KRISHNA-5478 <krishnatripathi07042005@gmail.com>
2026-01-27 22:14:29 +0530

    first commit

---
+++ nextgen-portfolio/sanity/schemaTypes/index.ts
@@ -0,0 +1,27 @@
+import type { SchemaTypeDefinition } from "sanity";
+import achievement from "./achievement";
+import certification from "./certification";
+import contact from "./contact";
+import education from "./education";
+import experience from "./experience";
+import navigation from "./navigation";
+import profile from "./profile";
+import project from "./project";
+import siteSettings from "./siteSettings";
+import skill from "./skill";
+
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [
+    profile,
+    project,
+    skill,
+    experience,
+    education,
+    certification,
+    achievement,
+    contact,
+    siteSettings,
+    navigation,
+  ],
+};
