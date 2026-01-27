62ad705dfbf76fb8f3bd02337fd3e638c3b14714
KRISHNA-5478 <krishnatripathi07042005@gmail.com>
2026-01-27 23:39:01 +0530

    Updated Backend

---
+++ nextgen-portfolio/app/studio/[[...tool]]/layout.tsx
@@ -0,0 +1,12 @@
+import react from 'react'
+function layout({children}:{children:React.ReactNode}) {
+    return(
+        <html lang="en">
+            <body>
+                {children}
+            </body>
+
+        </html>
+    )
+}
+export default layout
--- nextgen-portfolio/sanity.config.ts
+++ nextgen-portfolio/sanity.config.ts
@@ -1,14 +1,9 @@
 'use client'
-
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...tool]]\page.tsx` route
- */
-
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
+import {RocketIcon} from '@sanity/icons'
 
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
 import {schema} from './sanity/schemaTypes'
 import {structure} from './sanity/structure'
@@ -17,12 +12,14 @@ export default defineConfig({
   basePath: '/studio',
   projectId,
   dataset,
-  // Add and edit the content schema in the './sanity/schemaTypes' folder
+  title:"nextgen-portfolio",
+  subtitle:"Content Management System",
+  icon: RocketIcon,
   schema,
   plugins: [
-    structureTool({structure}),
+    structureTool({structure, title:"Content"}),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
+    visionTool({defaultApiVersion: apiVersion, title:"GROQ"}),
   ],
 })
